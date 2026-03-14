42db3a4d9328bb19ca670472cf7c7cd37eb7205e
lee <lee@ECHO>
2026-03-14 12:24:42 +0000

    12:24

---
+++ showWindows.js
@@ -0,0 +1,11 @@
+function showWindow(winID){
+    if(document.getElementById(winID).style.display=="block")
+    {
+        document.getElementById(winID).style.display="none";
+    }
+    else
+    {
+        document.getElementById(winID).style.display="block";
+    }
+
+}
